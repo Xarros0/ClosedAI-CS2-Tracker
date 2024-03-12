3b3ac454477bd499a55f4ef8e5998ffbb39b9421
Jeremia <103106203+Xarros0@users.noreply.github.com>
2024-03-12 14:37:20 +0200

    Update forumCatalog.tsx

--- src/components/forumCatalog.tsx
+++ src/components/forumCatalog.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MainNavbar from './mainNavbar';
 
-const forumCatalog: React.FC = () => {
+const ForumCatalog: React.FC = () => {
     const foreground: React.CSSProperties = {
         boxSizing: 'border-box',
         position: 'absolute',
@@ -13,15 +13,58 @@ const forumCatalog: React.FC = () => {
         marginLeft: '50px',
         marginRight: '50px',
         overflow: 'hidden',
+        padding: '20px', // Add padding for better spacing
     };
-  
+
+    const tableStyle: React.CSSProperties = {
+        marginTop: '30px', // Add margin to the top of the table
+        width: '100%', // Make table fill the container
+        borderCollapse: 'collapse', // Collapse borders between cells
+    };
+
+    const thStyle: React.CSSProperties = {
+        background: '#D9D9D9',
+        borderTop: '1px solid #000000',
+        borderBottom: '1px solid #000000',
+        padding: '10px',
+        textAlign: 'left',
+    };
+
+    const tdStyle: React.CSSProperties = {
+        borderTop: '1px solid #000000',
+        borderBottom: '1px solid #000000',
+        padding: '10px',
+        backgroundColor: '#FFFFFF', // Background color for each row
+    };
+
+    const linkStyle: React.CSSProperties = {
+        textDecoration: 'none',
+        color: '#000', // Set the color of the link
+    };
+
     return (
         <div style={foreground}>
             <MainNavbar />
-            <h1>Forum Catalog</h1>
-            <p>This is the forum catalog page</p>
+            <table style={tableStyle}>
+                <thead>
+                    <tr>
+                        <th style={thStyle}>Topic</th>
+                        <th style={thStyle}>Replies</th>
+                        <th style={thStyle}>Author</th>
+                        <th style={thStyle}>Activity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style={tdStyle}><a href="/forum" style={linkStyle}>Otsikko</a></td>
+                        <td style={tdStyle}>1</td>
+                        <td style={tdStyle}>user</td>
+                        <td style={tdStyle}>1 min ago</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
 
-export default forumCatalog;
+export default ForumCatalog;
